6259329ec798890297139716fa6a33a2d822eff9
Michal Miškerník <michal@miskernik.sk>
2016-09-21 17:07:23 +0100

    Initial commit

---
+++ myalloc.c
@@ -0,0 +1,18 @@
+/*	Stuart Norcross - 12/03/10 */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/time.h>
+
+#include "myalloc.h"
+
+
+
+void *myalloc(int size){
+	void *newRegion = malloc(size);
+	return newRegion;
+}
+
+void myfree(void *ptr){
+	free(ptr);
+}
